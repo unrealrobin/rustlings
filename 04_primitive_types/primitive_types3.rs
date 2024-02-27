093561aecc159c2cd5ec52ddf4fa2b3baac9bef7
Robin Lifshitz <Rlifshitz19@gmail.com>
2024-02-26 19:51:26 -0800

    Primitive Passing

--- 04_primitive_types/primitive_types3.rs
+++ 04_primitive_types/primitive_types3.rs
@@ -5,10 +5,11 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let a = ???
+    // Declare Array data type and length. Initialized to the i32 = 1, 100x;
+    let a: [i32; 100] = [1;100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
