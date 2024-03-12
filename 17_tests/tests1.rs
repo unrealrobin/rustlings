71c04c41e776b78e66961e59715d57f4e5582a52
Robin Lifshitz <Rlifshitz19@gmail.com>
2024-03-11 22:05:09 -0700

    Tests Passing

--- 17_tests/tests1.rs
+++ 17_tests/tests1.rs
@@ -10,12 +10,14 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        let num = 1;
+        let num2 = 2;
+        assert!(num != num2);
     }
 }
