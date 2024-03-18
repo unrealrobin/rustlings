d5f616ee509d1d86bc83901b1df2b97f50f84a09
Robin Lifshitz <Rlifshitz19@gmail.com>
2024-03-17 17:24:50 -0700

    Iterators Passing. Review .map(), .collect(), .for_each()

--- 18_iterators/iterators5.rs
+++ 18_iterators/iterators5.rs
@@ -11,7 +11,7 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -35,7 +35,17 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+
+    let mut count = 0;
+
+    map.values().for_each(|x| {
+        if x == &value {
+            count += 1
+        }
+    });
+
+    count
+    
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +64,17 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    let mut count = 0;
+
+    collection.iter().for_each(|hash|{
+        hash.values().for_each(|val| {
+            if *val == value {
+                count += 1;
+            }
+        })
+    });
+
+    count
 }
 
 #[cfg(test)]
